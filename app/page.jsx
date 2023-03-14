faeb4c6904067a7d3b6b488e9829a6d146c6012c
Mister Jantsan <sw10d222@gmail.com>
2023-03-14 22:06:26 +0800

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,25 @@
+import Movie from "./Movie"
+
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  )
+  const res = await data.json()
+  return (
+    <main>
+              <h1>Jantsan Just Movies </h1>
+      <div className="grid grid-cols-fluid gap-16">
+        
+        {res.results.map((movie) => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            poster_path={movie.poster_path}
+            release_date={movie.release_date}
+          />
+        ))}
+      </div>
+    </main>
+  )
+}
